f4fc572c00240bc3d3108c546fbdbe6dd7389eac
pascal-g38 <pascalgolomb@outlook.de>
2024-07-17 11:22:56 +0200

    Ausgabe der Liste + Suchleiste

--- webapp/controller/EmployeeList.controller.js
+++ webapp/controller/EmployeeList.controller.js
@@ -1,8 +1,24 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+
+], 
+    function (Controller, Filter, FilterOperator) {
     "use strict";
+
     return Controller.extend("ui5.uebungsprogramm.controller.EmployeeList", {
         
+		onFilterEmployees(oEvent) {
+			const aFilter = [];
+			const sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("EmployeeFirstName", FilterOperator.Contains, sQuery));
+			}
+
+			const oList = this.byId("employeeList");
+			const oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
+		    }
         });
 });
